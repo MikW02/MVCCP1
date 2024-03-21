5cb5e4b3b90f66cc28fdd7012faeb3ba1b720141
MikW02 <michaeleon1202@gmail.com>
2024-03-21 11:47:07 -0300

    Adicionar arquivos de projeto.

---
+++ 1403ASPNET/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _1403ASPNET.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
